010a17670856fd249fa18bb2e74bb0dad796753f
simluuuu <luca@slotta.org>
2025-04-08 10:26:34 +0200

    IMPORT textures + ADD JFrame application

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,5 +1,14 @@
 package src;
+import src.textures.*;
+
+import javax.swing.*;
+import java.awt.Image;
 
 public class Enemy {
 
+    Image image;
+
+    Enemy(){
+        image = new ImageIcon(getClass().getResource("./textures/image-5.png")).getImage();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 package src;
+import javax.swing.JFrame;
 
 public class Main {
 
+    private JFrame frame;
+    public Enemy enemy;
+
+    public static void main(String[] args) {
+        new Main().go();
+    }
+
+    private void go(){
+        frame = new JFrame("Tower Defense");
+
+        frame.setSize(1920, 1080);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setResizable(false);
+    }
 }
